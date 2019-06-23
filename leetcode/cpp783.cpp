d9d61f46b058edcfefec5bdef71d1ce5e0cdaa9c
knox <843762968@qq.com>
2019-06-23 13:54:19 +0800

    Minimum Distance Between BST Nodes

--- leetcode/cpp783.cpp
+++ leetcode/cpp783.cpp
@@ -11,10 +11,10 @@ class Solution {
 public:
     int res = INT_MAX, pre = -1;
     int minDiffInBST(TreeNode* root) {
-        if (root->left != NULL) minDiffInBST(root->left);
+        if (root->left) minDiffInBST(root->left);
         if (pre >= 0) res = min(res, root->val - pre);
         pre = root->val;
-        if (root->right != NULL) minDiffInBST(root->right);
+        if (root->right) minDiffInBST(root->right);
         return res;
     }
 };
