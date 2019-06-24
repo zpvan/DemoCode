0f8940143e70279cdf2b1d77fe43598bf4d21196
knox <843762968@qq.com>
2019-06-25 04:36:41 +0800

    Top K Frequent Words

--- leetcode/cpp692.cpp
+++ leetcode/cpp692.cpp
@@ -1,57 +1,28 @@
-#include <map>
-
-using namespace std;
-
 class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        map<string, int> wordsMap;
+        unordered_map<string, int> wordsMap;
+        for (auto word : words) 
+            wordsMap[word] += 1;
         
-        for (vector<string>::iterator it = words.begin();
-             it != words.end(); it++) {
-            string word = *it;
-            auto it_wsm = wordsMap.find(word);
-            if (it_wsm != wordsMap.end()) {
-                // exist element
-                it_wsm->second++;
-            } else {
-                // new element
-                wordsMap.insert(pair<string, int>(word, 1));
-            }
-            
-        }
+        vector<pair<string, int>> sortWords;
+        _sortMap(wordsMap, sortWords);
         
-        vector<pair<string, int>> wordsSort;
-        sortMap(wordsMap, wordsSort);
-        
-        vector<string> topkWords;
-        int i = 0;
-        for (vector<pair<string, int>>::iterator it = wordsSort.begin();
-             it != wordsSort.end(); it++) {
-            if (i >= k) {
-                break;
-            }
-            topkWords.push_back(it->first);
-            i++;
-        }
-        
-        return topkWords;
+        vector<string> topWords;
+        if (k > sortWords.size()) k = sortWords.size();
+        for (int i = 0; i < k; i++)
+            topWords.push_back(sortWords[i].first);
+        return topWords;
     }
     
 private:
-    static bool cmp(pair<string, int>& p1, pair<string, int>& p2) {
-        if (p1.second != p2.second) {
-            return p1.second > p2.second;
-        } else {
-            return strcmp(p2.first.c_str(), p1.first.c_str()) >= 0;
-        }
+    static bool _cmp(pair<string, int>& p1, pair<string, int>& p2) {
+        return (p1.second != p2.second) ? (p1.second > p2.second) : (strcmp(p2.first.c_str(), p1.first.c_str()) >= 0);
     }
     
-    void sortMap(map<string, int>& m, vector<pair<string, int>>& v) {
-        for (map<string, int>::iterator it = m.begin(); it != m.end(); it++) {
-            v.push_back(*it);
-        }
-        
-        sort(v.begin(), v.end(), cmp);
+    void _sortMap(unordered_map<string, int> &m, vector<pair<string, int>>& v) {
+        for (auto p : m)
+            v.push_back(p);
+        sort(v.begin(), v.end(), _cmp);
     }
 };
