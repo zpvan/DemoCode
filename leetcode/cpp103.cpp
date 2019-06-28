f645c6119ce586c41ae1c0f158baeb4864d8147e
knox <843762968@qq.com>
2019-06-28 22:54:14 +0800

    Binary Tree Zigzag Level Order Traversal

--- leetcode/cpp103.cpp
+++ leetcode/cpp103.cpp
@@ -12,18 +12,14 @@ public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         deque<TreeNode*> dq;
         vector<vector<int>> vv;
-        if (!root) {
-            return vv;
-        }
+        if (!root) return vv;
         dq.push_back(root);
         _bfs(true, dq, vv);
         return vv;
     }
 private:
     void _bfs(bool l2r, deque<TreeNode*>& dq, vector<vector<int>>& vv) {
-        if (dq.empty()) {
-            return;
-        }
+        if (dq.empty()) return;
         vector<int> v;
         int size = dq.size();
         if (l2r) {
@@ -31,24 +27,16 @@ private:
                 TreeNode *n = dq.front();
                 dq.pop_front();
                 v.push_back(n->val);
-                if (n->left) {
-                    dq.push_back(n->left);
-                }
-                if (n->right) {
-                    dq.push_back(n->right);
-                }
+                if (n->left) dq.push_back(n->left);
+                if (n->right) dq.push_back(n->right);
             }
         } else {
             for (int i = size - 1; i >= 0; i--) {
                 TreeNode *n = dq.back();
                 dq.pop_back();
                 v.push_back(n->val);
-                if (n->right) {
-                    dq.push_front(n->right);
-                }
-                if (n->left) {
-                    dq.push_front(n->left);
-                }
+                if (n->right) dq.push_front(n->right);
+                if (n->left) dq.push_front(n->left);
             }
         }
         vv.push_back(v);
