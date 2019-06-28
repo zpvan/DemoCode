7c186bd3ca2338a739e09f9988ba71ee3a627f83
knox <843762968@qq.com>
2019-06-28 23:13:30 +0800

    Contains Duplicate III

--- leetcode/cpp220.cpp
+++ leetcode/cpp220.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        set<long> s;
         if (!k) return false;
+        set<long> s;
         for (int i = 0; i < nums.size(); i++) {
             if (s.size() > k) s.erase(s.find(nums[i - k - 1]));
-            auto itu = s.lower_bound(nums[i]);
-            if (itu != s.end() && (*itu) - nums[i] <= t) {
-                return true;
-            }
-
+            auto it = s.lower_bound(nums[i]);
+            if (it != s.end() && (*it) - nums[i] <= t) return true;
             s.insert(nums[i]);
-            
             auto l = s.find(nums[i]);
-            if (l != s.begin()) {
-                l--;
-                if (nums[i] - (*l) <= t) {
-                    return true;
-                } 
-            }
+            if ((l-- != s.begin()) && (nums[i] - (*l) <= t)) return true;
         }
         return false;
     }
