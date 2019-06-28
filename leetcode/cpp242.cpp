f93b04649de2f5e07a7955dd1a24645358120626
zpvan <843762968@qq.com>
2019-06-28 09:49:50 +0800

    Valid Anagram

--- leetcode/cpp242.cpp
+++ leetcode/cpp242.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {        
-        unordered_map<char, int> ms;
-        unordered_map<char, int> mt;
-        
-        for (const auto& c : s) ms[c] += 1;
-        
-        for (const auto& c : t) mt[c] += 1;
-        
-        for (const auto& e : ms)
-            if (e.second != mt[e.first]) return false;
-        
-        for (const auto& e : mt)
-            if (e.second != ms[e.first]) return false;
-        
-        return true;
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> ms, mt;
+        for (auto c : s) ms[c] += 1;
+        for (auto c : t) mt[c] += 1;
+        for (auto p : ms) {
+            if (p.second != mt[p.first]) return false;
+            mt.erase(p.first);
+        }
+        return mt.empty();
     }
 };
