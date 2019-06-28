af7aeecdc74a0141c05c89dfc7e24a9081efe74e
zpvan <843762968@qq.com>
2019-06-28 09:27:29 +0800

    Two Sum

--- leetcode/cpp1.cpp
+++ leetcode/cpp1.cpp
@@ -3,10 +3,9 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> m;
         for (int i = 0; i < nums.size(); i++) {
-            auto it = m.find(target - nums[i]);
-            if (it != m.end()) return {it->second, i};
+            if (m.find(target - nums[i]) != m.end()) return {m.find(target - nums[i])->second, i};
             m.insert(pair<int, int>(nums[i], i));
         }
         return {};
-    }
+    }  
 };
