e630a2568c31fd2d06b6c1d3e4f6ba04cef57c3c
knox <843762968@qq.com>
2019-06-28 22:22:17 +0800

    Symmetric Tree

--- leetcode/cpp101.cpp
+++ leetcode/cpp101.cpp
@@ -9,15 +9,13 @@
  */
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
-        return _isMirror(root, root);
-    }
-private:
     bool _isMirror(TreeNode* r1, TreeNode* r2) {
         if (!r1 && !r2) return true;
         if (!r1 || !r2) return false;
-        return (r1->val == r2->val) 
-            && _isMirror(r1->left, r2->right) 
-            && _isMirror(r1->right, r2->left);
+        return (r1->val == r2->val) && _isMirror(r1->left, r2->right) && _isMirror(r1->right, r2->left);
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        return _isMirror(root, root);
     }
 };
