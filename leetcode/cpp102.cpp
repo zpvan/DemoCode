16b51dacf2a21b24990c4ad2070d973596d44b17
knox <843762968@qq.com>
2019-06-28 22:57:54 +0800

    Binary Tree Level Order Traversal

--- leetcode/cpp102.cpp
+++ leetcode/cpp102.cpp
@@ -12,28 +12,18 @@ public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         queue<TreeNode *> q;
         vector<vector<int>> vv;
-
-        if (NULL == root) {
-            return vv;
-        }
+        if (!root) return vv;
         q.push(root);
         while (1) {
-            if (q.empty()) {
-                break;
-            }
+            if (q.empty()) break;
             vector<int> v;
             int cnt = q.size();
             for (int i = 0; i < cnt; i++) {
                 TreeNode *cur = q.front();
                 q.pop();
-            
                 v.push_back(cur->val);
-                if (NULL != cur->left) {
-                    q.push(cur->left);
-                }
-                if (NULL != cur->right) {
-                    q.push(cur->right);
-                }
+                if (cur->left) q.push(cur->left);
+                if (cur->right) q.push(cur->right);
             }
             vv.push_back(v);
         }
